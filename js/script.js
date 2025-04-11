f356054643f26766e7bec7fb55e08314dfc37da9
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-04-11 16:33:01 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+//############################################################################
+//MAIN
+//############################################################################
+let rimaningTime = 5;
+const countdownManagement = document.getElementById("countdown");
+const numberListEl = document.querySelectorAll("#numbers-list", "li");
+const randomNumbers = Math.floor(Math.random() * 50) + 1;
+
+//############################################################################
+//FUNCTIONS
+//############################################################################
+
+function tempoScaduto() {
+  console.log("Tempo scaduto. Indovina i numero.");
+}
+
+const thirtySecondIntervalId = setInterval(() => {
+  rimaningTime -= 1;
+  countdownManagement.innerHTML = rimaningTime;
+  console.log(countdownManagement);
+
+  if (rimaningTime === 0) {
+    clearInterval(thirtySecondIntervalId);
+    tempoScaduto();
+  }
+}, 1000);
+
+numberListEl.forEach((li) => {
+  li.innerHTML += randomNumbers;
+});
